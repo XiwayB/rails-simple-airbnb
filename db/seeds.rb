8c76adeb2e066a04ad56d4bab7e1b15c8d9c05b6
XiwayBanh <xiway.banh@gmail.com>
2021-03-31 18:03:29 +0800

    added css style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+4.times do
+  Flat.create!(name:Faker::Movies::HarryPotter.character, address: Faker::Address.street_address, description: Faker::ChuckNorris.fact, price_per_night: rand(1..100), number_of_guests: rand(1..100))
+end
